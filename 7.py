23952df2532246ca36937d2d103b01205c206753
gulshat910 <gulsatdulatova02@gmail.com>
2025-09-13 15:29:51 +0300

    Create 7.py

---
+++ 7.py
@@ -0,0 +1,9 @@
+import math
+def calculate_circle_area(radius):
+    area = math.pi * radius ** 2
+    return area
+def is_positive(number):
+    if number > 0:
+        return True
+    else:
+        return False
