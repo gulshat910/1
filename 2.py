e03aada0d499f49ab9ff6a7573090a713c2de8bb
gulshat910 <gulsatdulatova02@gmail.com>
2025-09-13 13:54:37 +0300

    Create 2.py

---
+++ 2.py
@@ -0,0 +1,6 @@
+number = int(input("Введите число:"))
+if number % 5 == 0 and number % 10 == 0:
+    print("Число кратно и 5,и 10")
+else:
+    print("Число не кратно  одновременно 5 и 10")
+print()
