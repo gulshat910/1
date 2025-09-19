02e84cab365fe43ace1259f7e2cb080d8af43546
gulshat910 <gulsatdulatova02@gmail.com>
2025-09-19 14:32:12 +0500

    Update 7.py

--- 7.py
+++ 7.py
@@ -1,9 +1,20 @@
 import math
+
 def calculate_circle_area(radius):
-    area = math.pi * radius ** 2
-    return area
+    return math.pi * radius * radius
+
 def is_positive(number):
     if number > 0:
         return True
     else:
         return False
+
+# Демонстрация работы функций
+r = 3
+print("Площадь круга с радиусом", r, "равна", calculate_circle_area(r))
+
+num = -2
+print(num, "положительное?", is_positive(num))
+
+num = 4
+print(num, "положительное?", is_positive(num))
