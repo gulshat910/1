f1bdcdaef8c6739d6a20108d94f1da2384f72c17
gulshat910 <gulsatdulatova02@gmail.com>
2025-09-25 21:55:27 +0500

    Update 8.py

--- 8.py
+++ 8.py
@@ -1,1 +1,11 @@
+def shift_string(s):
+    out = ""
+    for ch in s:
+        if ch.isalpha():
+            base = 'A' if ch.isupper() else 'a'
+            out += chr((ord(ch) - ord(base) + 3) % 26 + ord(base))
+        else:
+            out += ch
+    return out
 
+print(shift_string(input("Введите строку: ")))
